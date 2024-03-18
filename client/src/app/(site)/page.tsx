8e37629757d55c7e58816e9ad01a265e58cc9180
Sreesanth <sreesanth46@gmail.com>
2024-03-18 07:48:07 +0530

    feat: home page

--- client/src/app/(site)/page.tsx
+++ client/src/app/(site)/page.tsx
@@ -1,11 +1,33 @@
+import Card from '@/components/Card';
 import React from 'react';
+import axios from 'axios';
 
-function HomePage() {
-  fetch('https://fakestoreapi.com/products/1')
-    .then(res => res.json())
-    .then(json => console.log(json));
+const getProducts = async () => {
+  const response = await fetch('https://fakestoreapi.com/products', {
+    method: 'GET'
+  });
 
-  return <div>HomePage</div>;
+  return response.json();
+};
+
+const createUser = async () => {
+  try {
+    const res = await axios.post('/api/products');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function HomePage() {
+  const products: IProduct[] = await getProducts();
+  await createUser();
+  return (
+    <div className="flex gap-4 flex-col md:flex-row md:flex-wrap justify-center items-center">
+      {products.map(product => (
+        <Card key={product.id} {...product} />
+      ))}
+    </div>
+  );
 }
 
 export default HomePage;
